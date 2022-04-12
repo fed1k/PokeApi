f29244bbeaee641c43ec4433ce464921fe354502
Retky <jose.arredondo.a@outlook.com>
2022-04-12 15:38:04 -0500

    Modify 'pokeDesc' to return an object with Api filtered data

--- src/modules/descript.js
+++ src/modules/descript.js
@@ -1,12 +1,16 @@
-const pokeDesc = [
-  {
-    name: 'Pikachu',
-    type: 'Electric',
-    category: 'Mouse',
-    abilities: 'Static',
-    height: '1\'04"',
-    weight: '13.2 lbs',
-  },
-];
+import getPoke from './pokeApi.js';
 
-export default pokeDesc;
+const getPokeDesc = async (pokeId) => {
+  const poke = await getPoke(pokeId)
+  const pokemon = await {
+    image: poke.sprites.front_default,
+    name: poke.name,
+    type: poke.types[0].type.name,
+    abilities: poke.abilities[0].ability.name,
+    height: `${poke.height/10} m`,
+    weight: `${poke.weight/10} kg`,
+  }
+  return pokemon
+};
+
+export default getPokeDesc;
