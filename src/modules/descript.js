d96c5c3f97521088ea4404557abadb33f4f5d272
Retky <jose.arredondo.a@outlook.com>
2022-04-12 09:47:23 -0500

    Add 'description' temp array

---
+++ src/modules/descript.js
@@ -0,0 +1,12 @@
+const pokeDesc = [
+  {
+    name: 'Pikachu',
+    type: 'ELectric',
+    category: 'Mouse',
+    abilities: 'Static',
+    height: '1\'04"',
+    weight: '13.2 lbs',
+  }
+]
+
+export default pokeDesc;
