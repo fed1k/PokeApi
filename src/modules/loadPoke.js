f9758c17fd4252ee3f6d5e4b73207435005b615e
Augusto <jesus_augga_99@icloud.com>
2022-04-13 14:32:39 -0500

    comments and reservations are shown correctly

--- src/modules/loadPoke.js
+++ src/modules/loadPoke.js
@@ -1,4 +1,5 @@
 import getPokeDesc from './descript.js';
+import { getInfo } from './APIcall.js';
 
 const loadPopup = (pokeId, type) => {
   getPokeDesc(pokeId)
@@ -15,22 +16,42 @@ const loadPopup = (pokeId, type) => {
       // Descript
       const pokeInfo = document.createElement('div');
       pokeInfo.classList = 'rsvDiv info';
-      pokeInfo.innerHTML = `<h2 class="pokeName">${poke.name}</h2>
-    <ul class="description">
-      <li class="descEl"><span class="attName">Type</span><span class="attValue">${poke.type}</span></li>
-      <li class="descEl"><span class="attName">Abilities</span><span class="attValue">${poke.abilities}</span></li>
-      <li class="descEl"><span class="attName">Height</span><span class="attValue">${poke.height}</span></li>
-      <li class="descEl"><span class="attName">Weight</span><span class="attValue">${poke.weight}</span></li>
-    </ul>`;
+      pokeInfo.innerHTML = `
+      <h2 class="pokeName">${poke.name}</h2>
+      <ul class="description">
+        <li class="descEl"><span class="attName">Type</span><span class="attValue">${poke.type}</span></li>
+        <li class="descEl"><span class="attName">Abilities</span><span class="attValue">${poke.abilities}</span></li>
+        <li class="descEl"><span class="attName">Height</span><span class="attValue">${poke.height}</span></li>
+        <li class="descEl"><span class="attName">Weight</span><span class="attValue">${poke.weight}</span></li>
+      </ul>
+      `;
 
       // Reservations
       const pokeReserv = document.createElement('div');
       pokeReserv.classList = 'rsvDiv reservation';
-      pokeReserv.innerHTML = `<h3>${type}s <span class="rsvCount">(2)</span></h3>
-      <ul class="rsvList">
-        <li>03/11/2021 - 03/12/2021 by Alex</li>
-        <li>03/14/2021 - 03/16/2021 by Mia</li>
-      </ul>`;
+      const ReservTitle = document.createElement('h3');
+
+      const rsvList = document.createElement('ul');
+      rsvList.className = 'rsvList';
+
+      const infoType = type === 'Comment' ? '/comments' : '/reservations';
+
+      getInfo(poke.name, infoType).then((items) => {
+        const itemsCount = items.length !== undefined ? items.length : 0;
+        ReservTitle.innerHTML = `${type}s (<span class="rsvCount">${itemsCount}</span>)`;
+        items.forEach((item) => {
+          let itemContainer = '';
+          if (infoType === '/comments') {
+            itemContainer = `<li>${item.creation_date} ${item.username}: ${item.comment}</li>`;
+          } else {
+            itemContainer = `<li>${item.date_start} - ${item.date_end} by ${item.username}</li>`;
+          }
+          rsvList.innerHTML += itemContainer;
+        });
+      });
+
+      pokeReserv.appendChild(ReservTitle);
+      pokeReserv.appendChild(rsvList);
 
       if (type === 'Comment') {
         loadCont[0].appendChild(pokeImage);
@@ -41,6 +62,9 @@ const loadPopup = (pokeId, type) => {
         loadCont[1].appendChild(pokeInfo);
         loadCont[1].appendChild(pokeReserv);
       }
+
+      const rsvCount = pokeReserv.querySelector('rsvCount');
+      rsvCount.innerHTML = 5;
     });
 };
 
