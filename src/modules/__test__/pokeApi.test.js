6bf091e978a7ed8d8e5af00d65c57aab60f1f792
Retky <jose.arredondo.a@outlook.com>
2022-04-13 17:23:07 -0500

    add 'pokeApi' fucntion jTest

---
+++ src/modules/__test__/pokeApi.test.js
@@ -0,0 +1,23 @@
+import getPoke from '../pokeApi.js';
+
+beforeAll(() => {
+  global.fetch = jest.fn(() => {
+    Promise.resolve({
+      json: () => Promise.resolve({name: "pikachu", height: 40}),
+    })
+  });
+});
+
+describe('getPoke Api call', () => {
+  test('Getting data form Api', async () => {
+    const getData = await getPoke('pikachu');
+
+    expect(getData).toBeDefined();
+  });
+
+  test('Correct fetch URL', async () => {
+    const getData = await getPoke('pikachu');
+
+    expect(fetch).toHaveBeenCalledWith('https://pokeapi.co/api/v2/pokemon/pikachu/');
+  })
+});
